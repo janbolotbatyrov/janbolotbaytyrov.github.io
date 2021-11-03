517130955e649690e552208d7ca1bf02c0dad31e
Janbolot Batyrov <janbolotbatyrovoff@.com>
2021-11-03 21:16:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { Route } from "react-router";
+import axios from "axios";
+import { Header } from "./components";
+import { Home, Cart } from "./pages";
+
+function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/db.json").then(({ data }) => { 
+      setPizzas(data.pizzas)
+     })
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
+        <Route path="/cart" component={Cart} exact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
